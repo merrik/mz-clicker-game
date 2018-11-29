7ab9ab5b32bd6d7dace819c04bda4cdf2fad7fa6
Archarious <archarious@gmail.com>
2018-11-29 15:56:10 +0300

    WIP: analytics; add calls in actions

--- src/analytics.js
+++ src/analytics.js
@@ -4,7 +4,7 @@ let startTime = 0;
 const GAGameStart = () => {
   if (window.ga) {
     window.ga('send', 'event', GAME_NAME, 'start_game');
-    
+
     startTime = Date.now();
     setInterval(() => {
       const currentTime = parseInt((Date.now() - startTime) / 1000);
@@ -15,6 +15,7 @@ const GAGameStart = () => {
   }
 };
 
+//C.ADD_MATERIAL
 const GAClicks = (count) => {
     if (window.ga) {
         window.ga('send', 'event', GAME_NAME, 'game_clics', count.toString());
@@ -23,7 +24,46 @@ const GAClicks = (count) => {
     }
 };
 
+const GAFirstBuy = (item) => {
+    if (window.ga) {
+        window.ga('send', 'event', GAME_NAME, 'game_first_buy', item);
+    } else {
+        console.error('No GA in WINDOW')
+    }
+};
+
+//C.BUY_UPGRADE
+const GABuy = (item) => {
+    if (window.ga) {
+        window.ga('send', 'event', GAME_NAME, 'game_buy', item);
+    } else {
+        console.error('No GA in WINDOW')
+    }
+};
+
+//C.SET_SHOWED_SHARE_BANNER
+const GAReachedStage = (stageIndex) => {
+    if (window.ga) {
+        window.ga('send', 'event', GAME_NAME, 'game_stage_reached', stageIndex.toString());
+    } else {
+        console.error('No GA in WINDOW')
+    }
+};
+
+//C.RESET_GAME
+const GAGameRestart = (stageIndex) => {
+    if (window.ga) {
+        window.ga('send', 'event', GAME_NAME, 'game_restarted');
+    } else {
+        console.error('No GA in WINDOW')
+    }
+};
+
 export {
   GAGameStart,
   GAClicks,
+  GAFirstBuy,
+  GABuy,
+  GAReachedStage,
+  GAGameRestart
 }
